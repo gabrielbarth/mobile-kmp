5a5a4696a977f5b517d11a3f5a46d15b576bb153
gabrielbarth <gabriel.silverio@stone.com.br>
2024-05-01 17:52:59 -0400

    feat: add dependencies

--- KotlinProject/composeApp/build.gradle.kts
+++ KotlinProject/composeApp/build.gradle.kts
@@ -39,6 +39,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
         }
     }
 }
